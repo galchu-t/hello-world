4592751207f82d61de6de6704e189e33a3ebffd0
Galtal-sharp <gal.talmor.box@gmail.com>
2024-07-13 13:11:26 +0300

    Initial commit

---
+++ GalHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GalHelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
